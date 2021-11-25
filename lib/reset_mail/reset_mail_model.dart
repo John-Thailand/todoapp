4b30563ae9bebc31d8ccf0b953b203d9e38a9824
John-Thailand <hamoban69@gmail.com>
2021-11-25 10:15:22 +0900

    メールアドレス変更

--- lib/reset_mail/reset_mail_model.dart
+++ lib/reset_mail/reset_mail_model.dart
@@ -67,60 +67,60 @@ class ResetMailModel extends ChangeNotifier {
 
     // 1. ユーザーの再認証を行う
     try {
-      _user.reauthenticateWithCredential(credential);
+      await _user.reauthenticateWithCredential(credential);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-mismatch') {
-        okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
         return false;
       } else if (e.code == 'user-not-found') {
-        okDialog(context, 'エラー', 'アカウントが見つかりません。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', 'アカウントが見つかりません。一度サインアウトしてやり直してください。');
         return false;
       } else if (e.code == 'invalid-credential') {
-        okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
         return false;
       } else if (e.code == 'invalid-email') {
-        okDialog(context, 'エラー', 'メールアドレスが正しくありません。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', 'メールアドレスが正しくありません。一度サインアウトしてやり直してください。');
         return false;
       } else if (e.code == 'user-disabled') {
-        okDialog(context, 'エラー', 'そのアカウントはご使用になれません。');
+        await okDialog(context, 'エラー', 'そのアカウントはご使用になれません。');
         return false;
       } else if (e.code == 'wrong-password') {
-        okDialog(context, 'エラー', 'パスワードが正しくありません。');
+        await okDialog(context, 'エラー', 'パスワードが正しくありません。');
         return false;
       } else if (e.code == 'too-many-requests') {
-        okDialog(context, 'エラー', 
+        await okDialog(context, 'エラー', 
           '認証失敗の回数が一定を超えました。'
           'しばらくして再度サインインしてください。'
         );
         return false;
       } else {
-        okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
         return false;
       }
     }
 
     // 2. メールアドレスをアップデートする
     try {
-      _user.updateEmail(newEmail!);
+      await _user.updateEmail(newEmail!);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'invalid-email') {
-        okDialog(context, 'エラー', 'メールアドレスが正しくありません。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', 'メールアドレスが正しくありません。一度サインアウトしてやり直してください。');
         return false;
       } else if (e.code == 'email-already-in-use') {
-        okDialog(context, 'エラー', '既にそのメールアドレスは使用されています。');
+        await okDialog(context, 'エラー', '既にそのメールアドレスは使用されています。');
         return false;
       } else if (e.code == 'requires-recent-login') {
-        okDialog(context, 'エラー', 'ログインしてから時間が経過しております。再度ログインしてください。');
+        await okDialog(context, 'エラー', 'ログインしてから時間が経過しております。再度ログインしてください。');
         return false;
       } else {
-        okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
+        await okDialog(context, 'エラー', '問題が発生しました。一度サインアウトしてやり直してください。');
         return false;
       }
     }
 
     // 3. メールアドレス確認用のメールを送信
-    _user.sendEmailVerification();
-    okDialog(context, '完了', '新しいメールアドレス宛にメールを送信しました。そちらからメールアドレスの認証を行ってください。');
+    await _user.sendEmailVerification();
+    await okDialog(context, '完了', '新しいメールアドレス宛にメールを送信しました。そちらからメールアドレスの認証を行ってください。');
 
     // 成功した場合はtrueを返す
     return true;
@@ -128,7 +128,7 @@ class ResetMailModel extends ChangeNotifier {
 
   // メールアドレスを更新
   Future updateEmailInFirestore() async {
-    FirebaseFirestore.instance.collection('users').doc(userId).update({
+    await FirebaseFirestore.instance.collection('users').doc(userId).update({
       'email': newEmail,
       'updatedTime': Timestamp.now(),
     });
