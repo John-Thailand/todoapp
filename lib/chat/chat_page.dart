7fe090c7c7d17616c8ca473dc38f88d2c5d68245
John-Thailand <hamoban69@gmail.com>
2021-12-13 21:21:04 +0900

    チャット画面の実装

--- lib/chat/chat_page.dart
+++ lib/chat/chat_page.dart
@@ -22,7 +22,77 @@ class ChatPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const Spacer(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 32.0,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 28.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(40),
+                              topLeft: Radius.circular(40),
+                              bottomLeft: Radius.circular(40),
+                            ),
+                            color: Colors.redAccent,
+                            gradient: LinearGradient(
+                                begin: FractionalOffset.topLeft,
+                                end: FractionalOffset.bottomRight,
+                                colors: [
+                                  const Color.fromARGB(255, 108, 132, 235),
+                                  const Color.fromARGB(255, 132, 199, 250),
+                                ],
+                                stops: const [
+                                  0.0,
+                                  1.0,
+                                ]),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Flutterって便利だね!',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 28.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            child: ClipOval(
+                              child: Image.asset('assets/images/account.png'),
+                            ),
+                          ),
+                          const SizedBox(width: 16.0),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color.fromARGB(255, 233, 233, 233),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text('凄い速度でUIが組み上がっていて楽しい！'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             textInputWidget(),
           ],
         ),
